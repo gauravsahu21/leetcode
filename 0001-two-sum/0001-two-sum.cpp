408cbefe86e0fe4e9a7a11b98f719b03200dff6d
Gaurav Kumar <60878374+gauravsahu21@users.noreply.github.com>
2023-06-26 22:47:37 +0530

    Time: 17 ms (59.73%), Space: 10.9 MB (31.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-         vector<int>ans;
         unordered_set<int>s;
-        s.insert(nums[0]);
-        int pos2=0,pos1=0;
-        for(int i=1;i<nums.size();i++)
+        int second;
+        vector<int>k;
+        for(int i=0;i<nums.size();i++)
         {
             if(s.find(target-nums[i])!=s.end())
-            {   pos1=*s.find(target-nums[i]);
-               pos2=i;
-                  break;
+            {   k.push_back(i);
+                second=*s.find(target-nums[i]);
+                 break;
             }
             s.insert(nums[i]);
-                
         }
-        for(int j=0;j<nums.size();j++)
+        for(int i=0;i<nums.size();i++)
         {
-            if(nums[j]==pos1)
-            { ans.push_back(j);
+            if(nums[i]==second)
+            {    k.push_back(i);
             break;
             }
             
         }
-        ans.push_back(pos2);
-        return ans;
+        
+        return k;
         
     }
 };
